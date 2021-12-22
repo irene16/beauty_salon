e2716281a62f5df1c067c4437559e9cd2afd9a50
irene16 <95614812+irene16@users.noreply.github.com>
2021-12-22 23:36:28 +0300

    Create but.js

---
+++ but.js
@@ -0,0 +1,23 @@
+function ToggleMenu(){
+    const button = document.getElementById('burger')
+    const menu = document.getElementById('menu')
+    
+    button.classList.toggle('active')
+    menu.classList.toggle('active')
+}
+
+function ToggleMenu(){
+    const button = document.getElementById('burger')
+    const menu = document.getElementById('menu')
+    
+    button.classList.toggle('active')
+    menu.classList.toggle('active')
+}
+
+function ToggleMenu(){
+    const button = document.getElementById('burger')
+    const menu = document.getElementById('menu')
+    
+    button.classList.toggle('active')
+    menu.classList.toggle('active')
+}
